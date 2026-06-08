a34cef95f3aa6be0e1149bebf7e6f586f9d36082
Ankith Joseph <ankithjoseph@hotmail.com>
2026-06-08 19:51:33 +0100

    minor fix

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,14 @@ export default {
 			fontFamily: {
 				sans: ['Inter', 'sans-serif'],
 			},
+			transitionTimingFunction: {
+				'out-quart': 'cubic-bezier(0.25, 1, 0.5, 1)',
+				'out-expo': 'cubic-bezier(0.16, 1, 0.3, 1)',
+			},
+			transitionDuration: {
+				'400': '400ms',
+				'600': '600ms',
+			},
 		},
 	},
 	plugins: [],
